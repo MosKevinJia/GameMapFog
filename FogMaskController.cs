5881156b82d3be106043a2972a91c356ec4dfbef
KevinJia <arivdjia@163.com>
2023-04-17 10:14:37 +0800

    computer shader
    computer shader

--- FogMaskController.cs
+++ FogMaskController.cs
@@ -7,20 +7,20 @@ using System.IO;
 
 public class FogMaskController : MonoBehaviour
 { 
-    public Image ui_fog;
+    public RawImage ui_fog;
     public Image ui_map;
+
+    public Texture2D circleMask;
+    private RenderTexture rtCircleMask;
+    public ComputeShader fogMaskComputeShader; 
 
     private Vector2 offset;
     public Vector2 map_size = new Vector2(100, 100);  // 在World中的实际大小   地图中心点为(0,0,0)
-
+     
     public Transform target;
     public Image player_mark;
-    public bool auto_align_mark;
-
-    public Color fogColor = Color.black;
-
-    public float fogSmoothRadius = 10f;
-    public int fogClearRadius = 50;
+    public bool auto_align_mark; 
+ 
     public bool MouseScrollWheel = true;
     [Range(0.5f, 3)]
     public float ScaleMin = 0.5f;
@@ -30,91 +30,122 @@ public class FogMaskController : MonoBehaviour
 
     public string save_path = "/fog.png";
 
-    private Texture2D fogMask;
-    private int width = 512;
-    private int height = 512;
-    private Color maskcolorStart;
-    private Color maskcolorEnd;
+    private RenderTexture fogMask;
+    private int width = 1204;
+    private int height = 1204;
+ 
     private Vector2 previousPlayerPosition = Vector2.zero;
     private float ui_map_width;
     private float ui_map_height;
     private ScrollRect sr;
-    
+    int kernelHandle;
 
     private void Start()
     {
         InitializeFogMask();
         StartCoroutine(UpdateFogMask()); 
+
         
     }
 
     private void InitializeFogMask()
     {
-        maskcolorStart = new Color(fogColor.r, fogColor.g, fogColor.b, 0);
-        maskcolorEnd = new Color(fogColor.r, fogColor.g, fogColor.b, 1);
+
+        rtCircleMask = new RenderTexture(circleMask.width / 2, circleMask.height / 2, 0);
+        RenderTexture.active = rtCircleMask;
+        Graphics.Blit(circleMask, rtCircleMask);
+        CreateFogMask(width, height);
+
+        kernelHandle = fogMaskComputeShader.FindKernel("UpdateFogMask");
+        // Set the shader variables
+        fogMaskComputeShader.SetTexture(kernelHandle, "fogMask", fogMask);
+        fogMaskComputeShader.SetTexture(kernelHandle, "circleMask", rtCircleMask);
+        fogMaskComputeShader.SetInts("fogMaskSize", new int[] { fogMask.width, fogMask.height }); 
+        
         ui_map_width = ui_map.rectTransform.rect.width;
         ui_map_height = ui_map.rectTransform.rect.height;
         sr = ui_map.GetComponentInParent<ScrollRect>();
 
         offset.x = map_size.x * 0.5f;
-        offset.y = map_size.y * 0.5f;
+        offset.y = map_size.y * 0.5f; 
 
-        fogMask = new Texture2D(width, height, TextureFormat.RGBA32, false);
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                fogMask.SetPixel(x, y, fogColor);
-            }
-        }
-        fogMask.Apply(); 
-        ui_fog.sprite = Sprite.Create(fogMask, new Rect(0, 0, width, height), Vector2.zero); 
-         
+        ui_fog.texture = fogMask; 
+
     }
 
+
+    private void CreateFogMask(int width, int height)
+    {
+        fogMask = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32); 
+        fogMask.enableRandomWrite = true; 
+        fogMask.Create();  
+        Graphics.Blit(circleMask, fogMask, new Vector2(0.001f, 0.001f), Vector2.zero); 
+    }
+
+
+    private void DrawCircleComputeShader(Vector2 vPos_Pct)
+    {
+        if (vPos_Pct == previousPlayerPosition)
+            return;
+
+        previousPlayerPosition = vPos_Pct; 
+
+        // Calculate the position of the circle mask in fogMask coordinates
+        int xPos = (int)(vPos_Pct.x * fogMask.width) - rtCircleMask.width / 2;
+        int yPos = (int)(vPos_Pct.y * fogMask.height) - rtCircleMask.height / 2; 
+       
+        fogMaskComputeShader.SetInts("maskPosition", new int[] { xPos, yPos }); 
+
+        // Dispatch the shader
+        int xGroups = Mathf.CeilToInt(rtCircleMask.width / 8.0f);
+        int yGroups = Mathf.CeilToInt(rtCircleMask.height / 8.0f);
+        fogMaskComputeShader.Dispatch(kernelHandle, xGroups, yGroups, 1); 
+         
+
+    }
      
 
     /// <summary>
     /// 画一个圆形空白区域[0 - 1]
     /// </summary>
     /// <param name="vPos_Pct"></param>
-    private void DrawCircle(Vector2 vPos_Pct)
-    {
-        if (vPos_Pct == previousPlayerPosition)
-            return;
+    //private void DrawCircle(Vector2 vPos_Pct)
+    //{
+    //    if (vPos_Pct == previousPlayerPosition)
+    //        return;
          
-        previousPlayerPosition = vPos_Pct;
-        int xPos = (int)(vPos_Pct.x * width);
-        int yPos = (int)(vPos_Pct.y * height);
+    //    previousPlayerPosition = vPos_Pct;
+    //    int xPos = (int)(vPos_Pct.x * width);
+    //    int yPos = (int)(vPos_Pct.y * height);
          
-        for (int y = yPos - fogClearRadius; y <= yPos + fogClearRadius; y++)
-        {
-            for (int x = xPos - fogClearRadius; x <= xPos + fogClearRadius; x++)
-            {
-                if (x >= 0 && x < width && y >= 0 && y < height)
-                {
-                    float distance = Vector2.Distance(new Vector2(x, y), new Vector2(xPos, yPos));
+    //    for (int y = yPos - fogClearRadius; y <= yPos + fogClearRadius; y++)
+    //    {
+    //        for (int x = xPos - fogClearRadius; x <= xPos + fogClearRadius; x++)
+    //        {
+    //            if (x >= 0 && x < width && y >= 0 && y < height)
+    //            {
+    //                float distance = Vector2.Distance(new Vector2(x, y), new Vector2(xPos, yPos));
 
-                    if (distance <= fogClearRadius - fogSmoothRadius)
-                    {
-                        fogMask.SetPixel(x, y, maskcolorStart);
-                    }
-                    else if (distance <= fogClearRadius)
-                    {
-                        Color curcolor = fogMask.GetPixel(x, y);
-                        // 计算插值因子
-                        float t = (distance - (fogClearRadius - fogSmoothRadius)) / fogSmoothRadius;
-                        // 根据插值因子计算颜色
-                        Color color = Color.Lerp(maskcolorStart, maskcolorEnd, t);
-                        Color color2 = new Color(color.r, color.g, color.b, curcolor.a - (1 - color.a));
-                        fogMask.SetPixel(x, y, color2);
-                    } 
+    //                if (distance <= fogClearRadius - fogSmoothRadius)
+    //                {
+    //                    fogMask.SetPixel(x, y, maskcolorStart);
+    //                }
+    //                else if (distance <= fogClearRadius)
+    //                {
+    //                    Color curcolor = fogMask.GetPixel(x, y);
+    //                    // 计算插值因子
+    //                    float t = (distance - (fogClearRadius - fogSmoothRadius)) / fogSmoothRadius;
+    //                    // 根据插值因子计算颜色
+    //                    Color color = Color.Lerp(maskcolorStart, maskcolorEnd, t);
+    //                    Color color2 = new Color(color.r, color.g, color.b, curcolor.a - (1 - color.a));
+    //                    fogMask.SetPixel(x, y, color2);
+    //                } 
 
-                }
-            }
-        } 
-        fogMask.Apply(); 
-    }
+    //            }
+    //        }
+    //    } 
+    //    fogMask.Apply(); 
+    //}
 
     private void LateUpdate()
     {
@@ -138,12 +169,13 @@ public class FogMaskController : MonoBehaviour
         while (true && target != null)
         { 
             Vector2 playerPosition = GetPlayerPositionOnMap();
-            DrawCircle(playerPosition);
+            //DrawCircle(playerPosition);
+            DrawCircleComputeShader(playerPosition);
             if (auto_align_mark)
             {
                 CenterPlayerMark();
             }
-           yield return new WaitForSeconds(0.5f);
+           yield return new WaitForSeconds(0.1f);
         }
     }
 
@@ -185,6 +217,17 @@ public class FogMaskController : MonoBehaviour
     }
 
 
+
+    private Texture2D toTexture2D(RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGBA32, false); 
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+        return tex;
+    }
+
+
     public void SaveFog(string file_path)
     {
         if (file_path == "")
@@ -192,10 +235,9 @@ public class FogMaskController : MonoBehaviour
             Debug.Log("error file_path: " + file_path);
             return;
         }
-
-
+         
         // 将纹理转换为PNG格式
-        byte[] pngData = fogMask.EncodeToPNG();
+        byte[] pngData = toTexture2D(fogMask).EncodeToPNG();
 
         // 检查PNG数据是否为空
         if (pngData != null)
@@ -219,7 +261,7 @@ public class FogMaskController : MonoBehaviour
     /// </summary>
     /// <param name="file_path"></param>
     public void LoadFog(string file_path)
-    { 
+    {
 
         // 获取文件路径
         string path = Path.Combine(Application.dataPath, file_path);
@@ -237,18 +279,20 @@ public class FogMaskController : MonoBehaviour
             if (texture.LoadImage(pngData))
             {
                 Debug.Log("Texture loaded from: " + path);
-                fogMask = texture;  
-                fogMask.Apply();
-                ui_fog.sprite = Sprite.Create(fogMask, new Rect(0, 0, width, height), Vector2.zero);
+
+                Graphics.Blit(texture, fogMask);
+                //fogMask = texture;
+                //fogMask.Apply();
+               // ui_fog.sprite = Sprite.Create(fogMask, new Rect(0, 0, width, height), Vector2.zero);
             }
             else
             {
-                Debug.LogError("Failed to load texture from: " + path); 
+                Debug.LogError("Failed to load texture from: " + path);
             }
         }
         else
         {
-            Debug.LogError("File not found: " + path); 
+            Debug.LogError("File not found: " + path);
         }
     }
 
